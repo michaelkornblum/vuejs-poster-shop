ba56f6019bfebb97c97df21e43105ba65ad9ae43
michaelkornblum <mkornblum71@gmail.com>
2018-07-30 21:47:45 -0500

    final product

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
 const price = 9.99
+const loadNum = 10;
 
 new Vue({
     el: '#app',
@@ -8,21 +9,35 @@ new Vue({
         cart: [],
         newSearch: 'anime',
         lastSearch: '',
+        results: [],
         loading: false,
         price,
     },
+    computed: {
+      noMoreItems: function() {
+        return this.items.length === this.results.length && this.results.length > 0;
+      },
+    },
     methods: {
+        appendItems: function() {
+            if(this.items.length < this.results.length) {
+                let append = this.results.slice(this.items.length, this.items.length + loadNum);
+                this.items = this.items.concat(append);
+            }
+        },
         onSubmit: function() {
-            this.items = [];
-            this.loading = true;
-            this.$http
-            .get('/search/'.concat(this.newSearch))
-            .then(function(res) {
-                this.lastSearch = this.newSearch;
-                console.log(res.data);
-                this.items = res.data;
-                this.loading = false;
-            });
+            if (this.newSearch.length) {
+                this.items = [];
+                this.loading = true;
+                this.$http
+                    .get('/search/'.concat(this.newSearch))
+                    .then(function(res) {
+                        this.lastSearch = this.newSearch;
+                        this.results = res.data;
+                        this.appendItems();
+                        this.loading = false;
+                });
+            }
         },
         addItem: function (index) {
             this.total += price;
@@ -53,6 +68,14 @@ new Vue({
         }
     },
     mounted: function() {
+        let viewInstance = this;
         this.onSubmit();
+        const elem = document.getElementById('product-list-bottom');
+        const watcher = scrollMonitor.create(elem);
+        watcher.enterViewport(function() { 
+            viewInstance.appendItems()
+        });
     }
 });
+
+
